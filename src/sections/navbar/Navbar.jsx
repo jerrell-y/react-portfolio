62f28615a81e1dfc1efb60b0b8032aedbe86f24a
Jerrell Yeoh <jerrellyeoh@gmail.com>
2024-08-21 14:17:51 +0800

    update

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import Logo from '../../assets/1.jpg'
 import data from './data'
+import { IoIosColorPalette } from "react-icons/io";
 import './navbar.css'
 
 const Navbar = () => {
@@ -14,7 +15,7 @@ const Navbar = () => {
             data.map(item => <li key={item.id}><a href={item.link}>{item.title}</a></li>)
           }
         </ul>
-        <button id='theme__icon'></button>
+        <button id='theme__icon'><IoIosColorPalette/></button>
       </div>
     </nav>
   )
